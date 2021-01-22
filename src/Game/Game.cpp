9324f14f81a8fc664de4f29a59898ad75606362d
alfa33333 <44150484+alfa33333@users.noreply.github.com>
2021-01-22 14:39:05 +1300

    update folder structure

---
+++ src/Game/Game.cpp
@@ -0,0 +1,129 @@
+#include "Game.h"
+#include "../Logger/Logger.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <glm/glm.hpp>
+#include <iostream>
+
+
+Game::Game() {
+    isRunning = false;
+    Logger::Log("Game constructor called");
+}
+
+Game::~Game() {
+    Logger::Log("Game destructor called");
+}
+
+void Game::Initialize() {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+        Logger::Err("Error initializing SDL.");
+        return;
+    }
+    SDL_DisplayMode displayMode;
+    SDL_GetCurrentDisplayMode(0, &displayMode);
+    windowWidth = displayMode.w;
+    windowHeight = displayMode.h;
+    window = SDL_CreateWindow(
+        NULL,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        windowWidth,
+        windowHeight,
+        SDL_WINDOW_BORDERLESS    
+    );
+    if (!window){
+        Logger::Err("Error creating SDL window.");
+        return;
+    }
+    renderer = SDL_CreateRenderer(window,-1, 0);
+    if (!renderer) {
+        Logger::Err("Error creating SDL renderer");
+    }
+
+    SDL_SetWindowFullscreen(window,SDL_WINDOW_FULLSCREEN);
+    isRunning = true;
+}
+
+void Game::Run() {
+    Setup();
+    while(isRunning) {
+        ProcessInput();
+        Update();
+        Render();
+    }
+}
+
+void Game::ProcessInput() {
+    SDL_Event sdlEvent;
+    while(SDL_PollEvent(&sdlEvent)) {
+        switch (sdlEvent.type)
+        {
+        case SDL_QUIT:
+            isRunning = false;
+            break;
+        
+        case SDL_KEYDOWN:
+            if (sdlEvent.key.keysym.sym == SDLK_ESCAPE){
+                isRunning = false;
+            }
+            break;
+        }
+    }
+}
+
+glm::vec2 playerPosition;
+glm::vec2 playerVelocity;
+
+void Game::Setup() {
+    playerPosition = glm::vec2(10.0, 20.0);
+    playerVelocity = glm::vec2(100.0, 0.0);
+}
+
+void Game::Update() {
+    //while (!SDL_TICKS_PASSED(SDL_GetTicks(), millisecsPreviousFrame + MILLISECS_PER_FRAME));
+    int timeTowait =  MILLISECS_PER_FRAME - (SDL_GetTicks()-millisecsPreviousFrame);    
+    if (timeTowait > 0 &&  timeTowait <= MILLISECS_PER_FRAME){
+        SDL_Delay(timeTowait);
+    }
+
+    //The differences in ticks since the last frame converted to seconds.
+    double deltaTime = (SDL_GetTicks()- millisecsPreviousFrame)/1000.0;
+
+    
+    millisecsPreviousFrame = SDL_GetTicks();
+
+
+    playerPosition.x +=  playerVelocity.x * deltaTime;
+    playerPosition.y +=  playerVelocity.y * deltaTime;
+}
+
+void Game::Render(){
+    SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
+    SDL_RenderClear(renderer);
+
+    //Draw a PNG texture
+    SDL_Surface* surface = IMG_Load("./assets/images/tank-tiger-right.png");
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+
+    //What is the destination 
+    SDL_Rect dstRect = {
+        static_cast<int>(playerPosition.x), 
+        static_cast<int>(playerPosition.y), 
+        32, 
+        32
+        };
+
+    SDL_RenderCopy(renderer, texture, NULL, &dstRect);
+
+    SDL_DestroyTexture(texture);
+
+    SDL_RenderPresent(renderer);
+}
+
+void Game::Destroy(){
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
-#include "Game.h"
-// #include <SDL2/SDL.h>
-// #include <SDL2/SDL_image.h>
-// #include <SDL2/SDL_ttf.h>
-// #include <SDL2/SDL_mixer.h>
-// #include <glm/glm.hpp>
-// #include <imgui/imgui.h>
-// #include <sol/sol.hpp>
+#include "./Game/Game.h"
+
 
 int main(int argc, char* arv[]) {
     Game game;
